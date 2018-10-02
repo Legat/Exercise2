3cad397b19c9809c9c4a90f90def1d98b6bb60ba
Patricul <Patricul@yandex.ru>
2018-10-02 21:29:09 +0300

    shedule commit

---
+++ app/src/main/java/com/example/peter/exercise2/Models/Category.java
@@ -0,0 +1,21 @@
+package com.example.peter.exercise2.Models;
+
+    public class Category {
+        private final int id;
+        private final String name;
+
+        public Category(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+
---
+++ app/src/main/java/com/example/peter/exercise2/Models/NewsItem.java
@@ -0,0 +1,45 @@
+package com.example.peter.exercise2.Models;
+
+import java.util.Date;
+
+public class NewsItem {
+    private final Category category;
+    private final String fullText;
+    private final String imageUrl;
+    private final String previewText;
+    private final Date publishDate;
+    private final String title;
+
+    public NewsItem(String title, String imageUrl, Category category, Date publishDate, String previewText, String fullText) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.category = category;
+        this.publishDate = publishDate;
+        this.previewText = previewText;
+        this.fullText = fullText;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public Date getPublishDate() {
+        return this.publishDate;
+    }
+
+    public String getPreviewText() {
+        return this.previewText;
+    }
+
+    public String getFullText() {
+        return this.fullText;
+    }
+}
